0519773ecf45385a7c855f021770b47c1537cf37
João Victor Fernandes de Araújo <joaovictorfernandesdearaujo@gmail.com>
2021-11-20 20:21:37 -0300

    Update dbpoo.sql

--- dbpoo.sql
+++ dbpoo.sql
@@ -1,13 +1,7 @@
--- Banco de dados: `dbpoo`
---
+
 CREATE DATABASE IF NOT EXISTS `dbpoo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `dbpoo`;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `netflix`
---
 
 DROP TABLE IF EXISTS `netflix`;
 CREATE TABLE IF NOT EXISTS `netflix` (
@@ -17,9 +11,17 @@ CREATE TABLE IF NOT EXISTS `netflix` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Extraindo dados da tabela `netflix`
---
 
 INSERT INTO `netflix` (`id`, `usuario`, `assistindo_no_momento`) VALUES
 (2, 'João', 'Iron Man');
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `login` (`nome`, `senha`) VALUES
+('João', 'Iron Man');
